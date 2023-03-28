7c0b9ebb86750d77a23e4d9e11573db09d2d61f3
Oscar Alcazar <oscar.alcazar.lobos@gmail.com>
2023-03-28 20:01:57 -0300

    Proyecto base creado

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [count, setCount] = useState(0)
+
+    return (
+        <div className="App">
+
+			<h1>Hola Mundo</h1>
+
+
+        </div>
+    )
+}
+
+export default App
